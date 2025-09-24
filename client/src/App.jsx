cccd0d122ce2b7cc1ac2cfd15bae25cedfeac24f
PriyansuRathore <priyansurathore255@gmail.com>
2025-09-25 03:41:23 +0530

    Fix: Remove duplicate Toaster component causing conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,12 +12,10 @@ import ReviewResume from './pages/ReviewResume'
 import Community from './pages/Community'
 import { useAuth } from '@clerk/clerk-react'
 import { useEffect } from 'react'
-import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   return (
     <div>
-      <Toaster/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/ai' element={<Layout/>}>
