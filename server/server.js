17b50fb841fd0cf0d81100af29b1b0af54988d36
PriyansuRathore <priyansurathore255@gmail.com>
2025-09-27 01:58:26 +0530

    Fix CORS policy for production frontend domain

--- server/server.js
+++ server/server.js
@@ -11,9 +11,10 @@ const app= express();
 await connectCloudinary()
 
 app.use(cors({
-    origin: '*',
+    origin: ['http://localhost:5173', 'https://quickai-coral.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }))
 app.use(express.json())
 app.use(clerkMiddleware())
