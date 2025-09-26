5f180d645e581d71eb342fbdcd7099d3ba94110f
PriyansuRathore <priyansurathore255@gmail.com>
2025-09-27 02:06:39 +0530

    Fix CORS: Allow all origins for PDF Chat

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app= express();
 await connectCloudinary()
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://quickai-coral.vercel.app'],
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
