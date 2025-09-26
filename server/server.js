cda2f2c21a616579777355b5b61935ead02f7ee3
PriyansuRathore <priyansurathore255@gmail.com>
2025-09-27 03:17:13 +0530

    Simplify CORS to basic setup

--- server/server.js
+++ server/server.js
@@ -10,27 +10,9 @@ const app= express();
 
 await connectCloudinary()
 
-app.use(cors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
-
-// Handle preflight requests
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*')
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    res.header('Access-Control-Allow-Credentials', 'true')
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200)
-    } else {
-        next()
-    }
-})
 
 app.get('/',(req,res)=>{
     res.send("Server is live");
